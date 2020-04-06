2e7241be76b6db3ba94598e9aeea644636f07289
wky <wky_0615@163.com>
2020-04-06 22:48:48 +0800

    Finish seele-todo

--- cli.js
+++ cli.js
@@ -1,13 +1,24 @@
 const {program} = require('commander');
 const api = require('./index.js');
 program
-    .option('-d, --debug', 'output extra debugging');
-
-program
     .command('add')
     .description('add a task')
     .action((...args) => {
         const words = args.slice(1)[0] && args.slice(1)[0].join(' ');
         api.add(words)
     });
+program
+    .command('clear')
+    .description('clear all tasks')
+    .action(() => {
+        api.clear()
+    });
+program
+    .command('show')
+    .description('show all tasks')
+    .action(() => {
+        api.showAll()
+    });
+
 program.parse(process.argv);
+
--- index.js
+++ index.js
@@ -1,7 +1,88 @@
 const db = require('./db');
+const inquirer = require('inquirer');
 
 module.exports.add = async (words) => {
     const list = await db.read();
     list.push({title: words, done: false});
     db.write(list);
+};
+
+module.exports.clear = async () => {
+    db.write([]);
+};
+
+module.exports.showAll = async () => {
+    const list = await db.read();
+    inquirer
+        .prompt(
+            {
+                type: 'list',
+                name: 'index',
+                message: 'Choose one task you want to operate.',
+                choices: [{
+                    name: 'Quit',
+                    value: '-1'
+                }, ...list.map((action, index) => {
+                    return {
+                        name: `${action.done ? '[√]' : '[_]'} ${index + 1} - ${action.title}`,
+                        value: index.toString()
+                    };
+                }), {name: '+ Create a new Task', value: '-2'}]
+            }
+        )
+        .then(answer => {
+            const index = parseInt(answer.index);
+            if (index >= 0) {
+                // 选中操作
+                inquirer.prompt({
+                    type: 'list',
+                    name: 'operation',
+                    message: 'What operation do you want?',
+                    choices: [
+                        {name: 'Quit', value: 'quit'},
+                        {name: 'Done', value: 'done'},
+                        {name: 'Undone', value: 'undone'},
+                        {name: 'Update', value: 'update'},
+                        {name: 'Delete', value: 'delete'},
+                    ]
+                }).then(option => {
+                    switch (option.operation) {
+                        case 'done':
+                            list[index].done = true;
+                            db.write(list);
+                            break;
+                        case 'undone':
+                            list[index].done = false;
+                            db.write(list);
+                            break;
+                        case 'update':
+                            inquirer.prompt({
+                                type: 'input',
+                                name: 'title',
+                                message: "New task title"
+                            }).then(input => {
+                                list[index].title = input.title;
+                                db.write(list);
+                            });
+                            break;
+                        case 'delete':
+                            list.splice(index, 1);
+                            db.write(list);
+                            break;
+                    }
+                });
+            } else if (index === -2) {
+                inquirer.prompt({
+                    type: 'input',
+                    name: 'title',
+                    message: "What is the title of your new task?"
+                }).then(input => {
+                    list.push({
+                        title: input.title,
+                        done: false,
+                    });
+                    db.write(list);
+                });
+            }
+        });
 };
